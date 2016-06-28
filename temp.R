f19fe162c5b89df973f55ac7405feff551158b5e
diardelavega <diardelavega@gmail.com>
2016-06-29 00:58:06 +0300

    kepping it up

--- temp.R
+++ temp.R
@@ -29,7 +29,7 @@ accuracy<- rep(NA, length(folds))
 
 
 gen_accuracy <- c();
-for(algorithm in c(#"bagging","rpart","randomForest",
+for(algorithm in c("bagging","rpart","randomForest",
   "PART","C50","J48",
   "naiveBayes","OneR","AdaBoostM1",  "svm", "JRip"
                    )){
@@ -94,27 +94,13 @@ headCrfv <-function(ho,algorithm,folds){
 
 
 
-f1  <- headOutcome~  t1+              
-  #t1Points+
-  t1Classification+ t1Form+          
-  t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+     
-  t1Atack+          t1Defense+        
-  t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+     
-  # t1AvgHtScoreIn+   t1AvgHtScoreOut+ 
-  # t1AvgFtScoreIn+   t1AvgFtScoreOut+  
-  #t1AvgHtGgResult+  t1AvgFtGgResult+  
-  #t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+     
-  t2+
-  #t2Points+       
-  t2Classification+ t2Form+ 
-  t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
-  t2Atack+          t2Defense+        
-  t2AtackIn+        t2AtackOut+       t2DefenseIn+      t2DefenseOut   
-  # t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut  
-  #t2AvgHtGgResult+  t2AvgFtGgResult+ 
-  #t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+      
-  #bet_1+            bet_X+            bet_2+       
-  # owd+  old+ odd+  mfd1+ mfd2
+f1  <- headOutcome~  t1+ t2+ mfd1+      mfd2+    t1Classification+ t2Classification+  pd+  fd+  
+  #f1d+ f2d+ f3d+ f4d+
+  t1adoe+          t2adoe+           t1e+             t2e+
+  owd+         odd+          old+    doav_ht+ doav_ft+
+  dwin+        dwout+       ddin+        ddout+       dlin+        dlout+    
+  datkin+      datkout+     ddefin+      ddefout+   dav_ftin+    dav_ftout+
+  bet_1+       bet_X+       bet_2+ bet_O+       bet_U
 
 
 
@@ -236,26 +222,48 @@ fullheadNoBet <- function(i){
     return(4)
   }
   
-  if(i==1){return(headOutcome~ 
-                    t1+               t1Points+         t1Classification+ t1Form+          
-                    t1Atack+          t1AtackIn+        t1AtackOut+       t1Defense+        t1DefenseIn+      t1DefenseOut+
-                    t1AvgHtScoreIn+   t1AvgHtScoreOut+  t1AvgFtScoreIn+   t1AvgFtScoreOut+  t1AvgHtGgResult+  t1AvgFtGgResult+
-                    t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
-                    t2+               t2Points+         t2Classification+ t2Form+           
-                    t2Atack+          t2AtackIn+        t2AtackOut+       t2Defense+        t2DefenseIn+      t2DefenseOut+    
-                    t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut+  t2AvgHtGgResult+  t2AvgFtGgResult+ 
-                    t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut)}
+  if(i==1){return(
+    f1  <- headOutcome~  #t1+              
+      #t1Points+
+      t1Classification+ t1Form+          
+      # t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+     
+      t1Atack+          t1Defense+        
+      # t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+     
+      # t1AvgHtScoreIn+   t1AvgHtScoreOut+ 
+      t1AvgFtScoreIn+   t1AvgFtScoreOut+  
+      #t1AvgHtGgResult+  t1AvgFtGgResult+  
+      t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
+      # t2+
+      #t2Points+       
+      t2Classification+ t2Form+ 
+      # t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
+      t2Atack+          t2Defense+        
+      # t2AtackIn+        t2AtackOut+       t2DefenseIn+      t2DefenseOut+    
+      # t2AvgHtScoreIn+   t2AvgHtScoreOut+ 
+      t2AvgFtScoreIn+   t2AvgFtScoreOut+
+      #t2AvgHtGgResult+  t2AvgFtGgResult+ 
+      t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+
+      #bet_1+            bet_X+            bet_2+       
+      owd+  old+ odd+  mfd1+ mfd2
+  )}
   
-  else if(i==2){return(headOutcome~ 
-                         t1+               t1Form+          
+  else if(i==2){return(headOutcome~  t1+              
+                         #t1Points+
+                         t1Classification+ t1Form+          
+                         t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+     
                          t1Atack+          t1Defense+        
-                         t1AvgHtScoreIn+   t1AvgFtScoreIn+   t1AvgFtGgResult+  #t1AvgHtGgResult+  
-                         t1WinsIn+         t1DrawsIn+        t1LosesIn+
-                         t2+               t2Form+           
+                         t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+     
+                         t1AvgHtScoreIn+   t1AvgHtScoreOut+ 
+                         t1AvgFtScoreIn+   t1AvgFtScoreOut+  
+                         #t1AvgHtGgResult+  t1AvgFtGgResult+  
+                         #t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+     
+                         t2+
+                         #t2Points+       
+                         t2Classification+ t2Form+ 
+                         t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
                          t2Atack+          t2Defense+        
-                         t2AvgHtScoreOut+  t2AvgFtScoreOut+  t2AvgFtGgResult+ #t2AvgHtGgResult+  
-                         t2WinsOut+        t2DrawsOut+       t2LosesOut
-                       #bet_O+            bet_U
+                         t2AtackIn+        t2AtackOut+       t2DefenseIn+      t2DefenseOut+    
+                         t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut  
   )}
   
   else if(i==3){return(headOutcome~ #mfd1+ mfd2+  
@@ -284,28 +292,29 @@ fullheadNoBet <- function(i){
                          t2LosesOut 
   )}
   
-  else if(i==4){return(headOutcome~  t1+              
-                         #t1Points+
-                         t1Classification+ t1Form+          
-                         t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+     
-                         t1Atack+          t1Defense+        
-                         t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+     
-                         t1AvgHtScoreIn+   t1AvgHtScoreOut+ 
-                         t1AvgFtScoreIn+   t1AvgFtScoreOut+  
-                         #t1AvgHtGgResult+  t1AvgFtGgResult+  
-                         #t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+     
-                         t2+
-                         #t2Points+       
-                         t2Classification+ t2Form+ 
-                         t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
-                         t2Atack+          t2Defense+        
-                         t2AtackIn+        t2AtackOut+       t2DefenseIn+      t2DefenseOut+    
-                         t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut+  
-                         #t2AvgHtGgResult+  t2AvgFtGgResult+ 
-                         #t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+      
-                         #bet_1+            bet_X+            bet_2+       
-                         owd+  old+ odd+  mfd1+ mfd2
-                       #bet_O+            bet_U
+  else if(i==4){return(
+    f1  <- headOutcome~  t1+              
+      #t1Points+
+      t1Classification+ t1Form+          
+      # t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+     
+      t1Atack+          t1Defense+        
+      # t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+     
+      # t1AvgHtScoreIn+   t1AvgHtScoreOut+ 
+      t1AvgFtScoreIn+   t1AvgFtScoreOut+  
+      #t1AvgHtGgResult+  t1AvgFtGgResult+  
+      t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
+       t2+
+      #t2Points+       
+      t2Classification+ t2Form+ 
+      # t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
+      t2Atack+          t2Defense+        
+      # t2AtackIn+        t2AtackOut+       t2DefenseIn+      t2DefenseOut+    
+      # t2AvgHtScoreIn+   t2AvgHtScoreOut+ 
+      t2AvgFtScoreIn+   t2AvgFtScoreOut+
+      #t2AvgHtGgResult+  t2AvgFtGgResult+ 
+      t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+
+      #bet_1+            bet_X+            bet_2+       
+      owd+  old+ odd+  mfd1+ mfd2
   )}
 }
 
