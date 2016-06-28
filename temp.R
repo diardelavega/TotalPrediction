6a24fdeee5644672802ab469d9faedbbe5221d41
diardelavega <diardelavega@gmail.com>
2016-06-28 19:22:53 +0300

    minor updates in header crfv, problems with some algorithms.

--- temp.R
+++ temp.R
@@ -1,20 +1,23 @@
 #libraries required
 {
   library(plyr)
+  library(randomForest)
+  library(rpart)
   library(e1071)  #svm
   library(C50)
-  library(randomForest)
+  
   library(ipred)
   library(RWeka)
-  library(rpart)
-  library(tree)
+  
+  # library(tree)
   #bestOfSize <- 3  
 }
 
 # "glm","svm","lm","bagging","Bagging" 
 # "C50","J48","svm","naiveBayes","randomForest","rpart","bagging", "PART","JRip","AdaBoostM1", "OneR"
 
-sampsize =dim(dfn)[1]*2/3
+
+sampsize =dim(dfn)[1]/6
 
 folds <- split(ndfn, cut(sample(1:nrow(ndfn),sampsize),10))
 folds <- split(dfn, cut(sample(1:nrow(dfn),sampsize),10))
@@ -26,12 +29,14 @@ accuracy<- rep(NA, length(folds))
 
 
 gen_accuracy <- c();
-for(algorithm in c("PART","naiveBayes","bagging"
-,"OneR","AdaBoostM1", "C50","J48","rpart", "svm", "JRip","randomForest"
+for(algorithm in c(#"bagging","rpart","randomForest",
+  "PART","C50","J48",
+  "naiveBayes","OneR","AdaBoostM1",  "svm", "JRip"
                    )){
   print(algorithm); set.seed(1234);
-  gen_accuracy[length(gen_accuracy)+1]<- headCrfv(fullheadBet(1),algorithm,folds)  }
+  gen_accuracy[length(gen_accuracy)+1]<- headCrfv(f1,algorithm,folds)  }
 mean(gen_accuracy)
+
 
 headCrfv <-function(ho,algorithm,folds){
   if(is.na(ho)){
@@ -87,24 +92,29 @@ headCrfv <-function(ho,algorithm,folds){
   return(100*mean(accuracy))
 }
 
-f1 <- headOutcome~ #mfd1+ mfd2+  odd+  old+  owd+
-  t1+               
-  
-  # t1Form+          t1Points+         t1Classification+
-  # t1Atack+          t1Defense+        
-  t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+
-  # t1AvgHtScoreIn+   t1AvgHtScoreOut+  t1AvgFtScoreIn+   t1AvgFtScoreOut+  
-  # t1AvgHtGgResult+  t1AvgFtGgResult+  
-  # t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
-  t2+               t2Points+         t2Classification+
-  # t2Form+           
-  # t2Atack+          t2Defense+        
-  t2DefenseIn+      t2DefenseOut+    t2AtackIn+        t2AtackOut+
-  # t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut+  
-  # t2AvgHtGgResult+  t2AvgFtGgResult+ 
-  # t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+      
-  bet_1+            bet_X+            bet_2
-  # bet_O+            bet_U
+
+
+f1  <- headOutcome~  t1+              
+  #t1Points+
+  t1Classification+ t1Form+          
+  t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+     
+  t1Atack+          t1Defense+        
+  t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+     
+  # t1AvgHtScoreIn+   t1AvgHtScoreOut+ 
+  # t1AvgFtScoreIn+   t1AvgFtScoreOut+  
+  #t1AvgHtGgResult+  t1AvgFtGgResult+  
+  #t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+     
+  t2+
+  #t2Points+       
+  t2Classification+ t2Form+ 
+  t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
+  t2Atack+          t2Defense+        
+  t2AtackIn+        t2AtackOut+       t2DefenseIn+      t2DefenseOut   
+  # t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut  
+  #t2AvgHtGgResult+  t2AvgFtGgResult+ 
+  #t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+      
+  #bet_1+            bet_X+            bet_2+       
+  # owd+  old+ odd+  mfd1+ mfd2
 
 
 
@@ -134,77 +144,90 @@ fullheadBet <- function(i){
                bet_1+            bet_X+            bet_2)         
   }
   
-  else if(i==2){return( headOutcome~ 
-                          t1+               t1Points+         t1Classification+ t1Form+          
-                          t1Atack+          t1AtackIn+        t1AtackOut+       t1Defense+        t1DefenseIn+      t1DefenseOut+
-                          t1AvgHtScoreIn+   t1AvgHtScoreOut+  t1AvgFtScoreIn+   t1AvgFtScoreOut+  t1AvgHtGgResult+  t1AvgFtGgResult+
-                          t1WinsIn+         t1WinsOut+       t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
-                          t2+               t2Points+         t2Classification+ t2Form+           
-                          t2Atack+          t2AtackIn+        t2AtackOut+       t2Defense+        t2DefenseIn+      t2DefenseOut+    
-                          t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut+  t2AvgHtGgResult+  t2AvgFtGgResult+ 
-                          t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+      
-                          bet_O+            bet_U)}
+  else if(i==2){return( headOutcome~ #mfd1+ mfd2+  odd+  old+  owd+
+                          # t1+               
+                          t1Form+          t1Points+         t1Classification+
+                          # t1Atack+          t1Defense+
+                          t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+
+                          # t1AvgHtScoreIn+   t1AvgHtScoreOut+  t1AvgFtScoreIn+   t1AvgFtScoreOut+  
+                          # t1AvgHtGgResult+  t1AvgFtGgResult+  
+                          # t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
+                          # t2+               
+                          t2Form+         t2Points+         t2Classification+
+                          # t2Atack+          t2Defense+
+                          t2DefenseIn+      t2DefenseOut+    t2AtackIn+        t2AtackOut+
+                          # t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut+  
+                          # t2AvgHtGgResult+  t2AvgFtGgResult+ 
+                          # t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+      
+                          bet_1+            bet_X+            bet_2)}
   
-  else if(i==3){return(headOutcome~ 
-                         t1+               t1Form+           #t1Points+         #t1Classification+
-                         t1Atack+          t1Defense+        
-                         t1AvgHtScoreIn+   t1AvgFtScoreIn+   t1AvgHtGgResult+  t1AvgFtGgResult+  
-                         t1WinsIn+         t1DrawsIn+        t1LosesIn+
-                         t2+               t2Form+            #t2Points+         #t2Classification+
-                         t2Atack+          t2Defense+        
-                         t2AvgHtScoreOut+  t2AvgFtScoreOut+  t2AvgHtGgResult+  t2AvgFtGgResult+ 
-                         t2WinsOut+        t2DrawsOut+       t2LosesOut+
-                         bet_O+            bet_U)}
+  else if(i==3){return(headOutcome~ #mfd1+ mfd2+  
+                         # odd+  old+  owd+
+                         t1+
+                         t1Form+          t1Points+         t1Classification+
+                         t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+
+                         t1Atack+          t1Defense+
+                         t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+
+                         t1AvgHtScoreIn+   t1AvgHtScoreOut+  t1AvgFtScoreIn+   t1AvgFtScoreOut+
+                         # t1AvgHtGgResult+  t1AvgFtGgResult+
+                         # t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
+                         t2+
+                         t2Form+           t2Points+         t2Classification+
+                         t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+
+                         t2Atack+          t2Defense+
+                         t2DefenseIn+      t2DefenseOut+    t2AtackIn+        t2AtackOut+
+                         t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut+
+                         # t2AvgHtGgResult+  t2AvgFtGgResult+
+                         # t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+
+                         bet_1+            bet_X+            bet_2+
+                         bet_O+            bet_U
+  )}
   
-  else if(i==4){return(headOutcome~#week+  
-                         t1+               
-                         #t1Points+         t1Classification+ t1Form+          
-                         t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+      
+  else if(i==4){return(headOutcome~ #mfd1+ mfd2+  
+                         # odd+  old+  owd+
+                         # t1+
+                         t1Points+         t1Classification+ t1Form+
+                         # t1Form1Diff+      t1Form2Diff+      #t1Form3Diff+      t1Form4Diff+      
                          t1AtackIn+        #t1AtackOut+        
                          t1DefenseIn+      #t1DefenseOut+     
-                         t1AvgHtScoreIn+   #t1AvgHtScoreOut+ 
-                         t1AvgFtScoreIn+   #t1AvgFtScoreOut+  
+                         # t1AvgHtScoreIn+   #t1AvgHtScoreOut+ 
+                         # t1AvgFtScoreIn+   #t1AvgFtScoreOut+  
                          #t1AvgHtGgResult+  t1AvgFtGgResult+  
                          t1WinsIn+         #t1WinsOut+       
                          t1DrawsIn+        #t1DrawsOut+       
                          t1LosesIn+        #t1LosesOut+       
-                         t2+               
-                         #t2Points+         t2Classification+ t2Form+           
-                         t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
+                         # t2+
+                         t2Points+         t2Classification+ t2Form+
+                         # t2Form1Diff+      t2Form2Diff+      #t2Form3Diff+      t2Form4Diff+     
                          t2AtackOut+       #t2AtackIn+               
                          t2DefenseOut+     #t2DefenseIn+      
-                         t2AvgHtScoreOut+  #t2AvgHtScoreIn+   
-                         t2AvgFtScoreOut+  #t2AvgFtScoreIn+   
+                         # t2AvgHtScoreOut+  #t2AvgHtScoreIn+   
+                         # t2AvgFtScoreOut+  #t2AvgFtScoreIn+   
                          #t2AvgHtGgResult+  t2AvgFtGgResult+ 
                          t2WinsOut+        #t2WinsIn+         
                          t2DrawsOut+       #t2DrawsIn+        
-                         t2LosesOut+       #t2LosesIn+        
-                         bet_O+            bet_U)}
+                         t2LosesOut+       #t2LosesIn+         t2LosesOut+
+                         bet_1+            bet_X+            bet_2)}
   
-  else if(i==5){return(headOutcome~  
-                         #t1+              
-                         #t1Points+
-                         #t1Classification+ t1Form+          
-                         t1Form1Diff+      t1Form2Diff+      #t1Form3Diff+      t1Form4Diff+     
-                         #t1Atack+          t1Defense+        
-                         t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+     
-                         #t1AvgHtScoreIn+   t1AvgHtScoreOut+ 
-                         t1AvgFtScoreIn+   t1AvgFtScoreOut+  
-                         #t1AvgHtGgResult+  t1AvgFtGgResult+  
-                         #t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+     
-                         #t2+
-                         #t2Points+       
-                         #t2Classification+ t2Form+ 
-                         t2Form1Diff+      t2Form2Diff+      #t2Form3Diff+      t2Form4Diff+     
-                         #t2Atack+          t2Defense+        
-                         t2AtackIn+        t2AtackOut+       t2DefenseIn+      t2DefenseOut+    
-                         #t2AvgHtScoreIn+   t2AvgHtScoreOut+  
-                         t2AvgFtScoreIn+   t2AvgFtScoreOut+
-                         #t2AvgHtGgResult+  t2AvgFtGgResult+ 
-                         #t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+      
-                         #bet_1+            bet_X+            bet_2+       
-                         owd+  old+ odd+  #mfd1+ mfd2
-                         bet_O+            bet_U)}
+  else if(i==5){return(headOutcome~ #mfd1+ mfd2+  
+                         odd+  old+  owd+
+                         t1+
+                         t1Form+          #t1Points+          t1Classification+
+                         t1Form1Diff+      t1Form2Diff+      #t1Form3Diff+      t1Form4Diff+
+                         # t1Atack+          t1Defense+
+                         t1AtackIn+        t1AtackOut+       t1DefenseIn+      t1DefenseOut+
+                         # t1AvgHtScoreIn+   t1AvgHtScoreOut+  t1AvgFtScoreIn+   t1AvgFtScoreOut+
+                         # t1AvgHtGgResult+  t1AvgFtGgResult+
+                         t1WinsIn+         t1WinsOut+        t1DrawsIn+        t1DrawsOut+       t1LosesIn+        t1LosesOut+
+                         t2+
+                         t2Form+           #t2Points+          t2Classification+
+                         t2Form1Diff+      t2Form2Diff+      #t2Form3Diff+      t2Form4Diff+
+                         # t2Atack+          t2Defense+
+                         t2DefenseIn+      t2DefenseOut+    t2AtackIn+        t2AtackOut+
+                         # t2AvgHtScoreIn+   t2AvgHtScoreOut+  t2AvgFtScoreIn+   t2AvgFtScoreOut+
+                         # t2AvgHtGgResult+  t2AvgFtGgResult+
+                         t2WinsIn+         t2WinsOut+        t2DrawsIn+        t2DrawsOut+       t2LosesIn+        t2LosesOut+
+                         bet_1+            bet_X+            bet_2)}
 }
 
 # 6 - 9 /4
@@ -235,32 +258,30 @@ fullheadNoBet <- function(i){
                        #bet_O+            bet_U
   )}
   
-  else if(i==3){return(headOutcome~week+  
-                         #t1+               
-                         #t1Points+         t1Classification+ 
-                         t1Form+          
-                         #t1Form1Diff+      t1Form2Diff+      t1Form3Diff+      t1Form4Diff+      
+  else if(i==3){return(headOutcome~ #mfd1+ mfd2+  
+                         # odd+  old+  owd+
+                         t1+
+                         t1Points+         t1Classification+ t1Form+
+                         # t1Form1Diff+      t1Form2Diff+      #t1Form3Diff+      t1Form4Diff+      
                          t1AtackIn+        #t1AtackOut+        
                          t1DefenseIn+      #t1DefenseOut+     
-                         t1AvgHtScoreIn+   #t1AvgHtScoreOut+ 
-                         t1AvgFtScoreIn+   #t1AvgFtScoreOut+  
+                         t1AvgHtScoreIn+   #t1AvgHtScoreOut+
+                         t1AvgFtScoreIn+   #t1AvgFtScoreOut+
                          #t1AvgHtGgResult+  t1AvgFtGgResult+  
                          t1WinsIn+         #t1WinsOut+       
                          t1DrawsIn+        #t1DrawsOut+       
                          t1LosesIn+        #t1LosesOut+       
-                         #t2+               
-                         #t2Points+         t2Classification+ 
-                         t2Form+           
-                         #t2Form1Diff+      t2Form2Diff+      t2Form3Diff+      t2Form4Diff+     
+                         t2+
+                         t2Points+         t2Classification+ t2Form+
+                         # t2Form1Diff+      t2Form2Diff+      #t2Form3Diff+      t2Form4Diff+     
                          t2AtackOut+       #t2AtackIn+               
                          t2DefenseOut+     #t2DefenseIn+      
-                         t2AvgHtScoreOut+  #t2AvgHtScoreIn+   
-                         t2AvgFtScoreOut+  #t2AvgFtScoreIn+   
+                         t2AvgHtScoreOut+  #t2AvgHtScoreIn+
+                         t2AvgFtScoreOut+  #t2AvgFtScoreIn+
                          #t2AvgHtGgResult+  t2AvgFtGgResult+ 
                          t2WinsOut+        #t2WinsIn+         
                          t2DrawsOut+       #t2DrawsIn+        
-                         t2LosesOut#+       #t2LosesIn+        
-                       #et_O+            bet_U
+                         t2LosesOut 
   )}
   
   else if(i==4){return(headOutcome~  t1+              
